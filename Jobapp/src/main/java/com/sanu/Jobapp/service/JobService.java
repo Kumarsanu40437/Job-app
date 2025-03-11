b1b7257cef5ab3a320479bafb64ababd05fd214f
Kumarsanu40437 <141845902+Kumarsanu40437@users.noreply.github.com>
2025-03-11 13:13:05 +0530

    Add files via upload

---
+++ Jobapp/src/main/java/com/sanu/Jobapp/service/JobService.java
@@ -0,0 +1,27 @@
+package com.sanu.Jobapp.service;
+
+import com.sanu.Jobapp.model.JobPost;
+import com.sanu.Jobapp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    @Autowired
+    private JobRepo repo;
+
+    // method to add a jobPost
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+
+    }
+
+
+    //method to return all JobPosts
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+
+    }
+    }
---
+++ Jobapp/src/test/java/com/sanu/Jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanu.Jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
